ef98b2640168dd7fe4823ba06f25d3042a3fc6fd
tanzid-48 <mdtanzid.525@gmail.com>
2026-05-18 22:11:45 +0600

    feat: implement POST /ideas  successfully insert user ideas into mongodb

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-require('dotenv').config();
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port =  process.env.PORT || 5000
-
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
 // Adds headers: Access-Control-Allow-Origin: *
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xhm3y2q.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,29 +17,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	
+    // Connect the client to the server
     await client.connect();
-   
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const db = client.db("IdeaVault");
+    const ideaVaultCollection = db.collection("ideas");
+
+    // post the idea
+    app.post("/ideas", async (req, res) => {
+      const newIdea = req.body;
+      const result = await ideaVaultCollection.insertOne(newIdea);
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
-   
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('IdeaVault-server is running');
-})
+app.get("/", (req, res) => {
+  res.send("IdeaVault-server is running");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
