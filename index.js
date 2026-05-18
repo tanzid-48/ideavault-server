0d14bc9fddc6b2155e36246017770fc6db2adbcd
tanzid-48 <mdtanzid.525@gmail.com>
2026-05-18 20:52:20 +0600

    Setup MongoDB connection

--- index.js
+++ index.js
@@ -10,6 +10,33 @@ app.use(cors())
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xhm3y2q.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	
+    await client.connect();
+   
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('IdeaVault-server is running');
