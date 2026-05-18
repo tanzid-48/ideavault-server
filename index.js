375e0c5177a05566ed0fef04b52326c91c84a7d2
tanzid-48 <mdtanzid.525@gmail.com>
2026-05-18 20:35:59 +0600

    Initial Express server setup

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port =  process.env.PORT || 5000
+
+
+// Adds headers: Access-Control-Allow-Origin: *
+app.use(cors())
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+  res.send('IdeaVault-server is running');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
